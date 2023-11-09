7da0a344d2f44cf2a1c496ba6203cb111411a206
Kapioga <ldaleyvo@gmail.com>
2023-11-09 18:18:22 -0500

    removed button nodes, starting fresh

--- Script/main_game.gd
+++ Script/main_game.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-@onready var buttons = $Buttons
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +9,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func logic():
 	
